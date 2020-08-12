63dc14135830b33650d9d48ea3d48dc4084bb986
huangqingfeng <15110160114@163.com>
2020-08-12 18:36:36 +0800

    1

--- src/main/java/com/example/keycloak/config/SecurityConfig.java
+++ src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -3,13 +3,21 @@ package com.example.keycloak.config;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
@@ -25,10 +33,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@Configuration
-//@EnableWebSecurity
-@KeycloakConfiguration
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Autowired
+    public KeycloakClientRequestFactory keycloakClientRequestFactory;
+
+    // 注册keycloakRestTemplate
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public KeycloakRestTemplate keycloakRestTemplate() {
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    }
 
     // 任务SimpleAuthorityMapper，以确保角色不以ROLE_为前缀
     @Autowired
--- src/main/java/com/example/keycloak/controller/HomeController.java
+++ src/main/java/com/example/keycloak/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.keycloak.controller;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,9 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private KeycloakRestTemplate template;
 
     // 公开页
     @GetMapping("/")
@@ -30,5 +36,17 @@ public class HomeController {
     public String logout(HttpServletRequest request) throws ServletException {
         request.logout();
         return "redirect:/";
+    }
+
+
+    // 私有方法
+    public KeycloakPrincipal getKeycloakPrinciple(HttpServletRequest request){
+        KeycloakPrincipal keycloakPrincipal = (KeycloakPrincipal)request.getUserPrincipal();
+        return keycloakPrincipal;
+    }
+
+    public String getTokenString(HttpServletRequest request){
+        String tokenString = getKeycloakPrinciple(request).getKeycloakSecurityContext().getTokenString();
+        return tokenString;
     }
 }
