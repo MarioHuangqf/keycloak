2b360f7687dfb69f0a5c833803695aac151ecb2a
huangqingfeng <15110160114@163.com>
2020-08-28 17:55:50 +0800

    1

--- src/test/java/com/example/keycloak/KeycloakApplicationTests.java
+++ src/test/java/com/example/keycloak/KeycloakApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.keycloak;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Parameter;
+import java.util.regex.Pattern;
+
 @SpringBootTest
 class KeycloakApplicationTests {
 
     @Test
     void contextLoads() {
+        boolean a = Pattern.matches("[A-Za-z0-9]{8,16}", "user123");
+        System.out.println(a);
     }
 
 }
